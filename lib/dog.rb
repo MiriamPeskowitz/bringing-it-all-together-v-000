b39ee0356c825ec4240357857109baae80437c6f
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-07 00:30:20 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -93,8 +93,7 @@ class Dog
   end
 
   def self.find_or_create_by(name:, breed:)
-    sql = "SELECT * FROM dogs WHERE name = ? AND breed = ?"
-
+  
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
 
     if !dog.empty?
