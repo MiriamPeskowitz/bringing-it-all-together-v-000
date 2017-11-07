c2c9f32f39ef48a226b457e1cfa2d1444eec1409
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-07 00:43:29 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -102,7 +102,7 @@ class Dog
 
         dog = self.new_from_db(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
         # Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
-      binding.pry
+    
     else
         dog = self.create(name: name, breed:breed )
     end
