cc853ca277541f36fd29f8baee693eafafd0bece
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-07 00:15:25 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -98,8 +98,8 @@ class Dog
     dog = DB[:conn].execute(sql, name, breed)
 
     if !dog.empty?
-        dog_data = dog[0]
-        dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
+        row = dog[0]
+        dog = Dog.new(row[0], row[1], row[2])
     else
         dog = self.create(name: name, breed: breed)
     end
