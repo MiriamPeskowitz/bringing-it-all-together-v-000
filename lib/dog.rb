f8fcb02f044671fc6538d9955f9f5d67be060d2e
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-07 00:01:49 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -85,7 +85,7 @@ class Dog
       self.new_from_db(row)
     end.first
     #first returns first el of array
-    #first(2) returns first 2 of array! 
+    #first(2) returns first 2 of array!
   end
 
   def self.find_or_create_by(name:, breed:)
